8b046b014c597583fbcb221d31ca6f86a7b94112
valioman <valioman@gmail.com>
2022-11-02 12:41:30 +0200

    remove stickiness of play alb

--- 02-play-app-load-balancer.tf
+++ 02-play-app-load-balancer.tf
@@ -44,7 +44,7 @@ resource "aws_lb_target_group" "play_target_group" {
   stickiness {
     type            = "lb_cookie"
     cookie_duration = 86400
-    enabled         = true
+    enabled         = false
   }
 
   health_check {
